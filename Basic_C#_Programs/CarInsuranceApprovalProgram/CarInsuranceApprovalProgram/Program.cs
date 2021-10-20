a56b300d5a9a6b5b749cb65b9f30531affa373bb
nicktheperkins <nicktheperkins@gmail.com>
2021-10-20 11:27:18 -0600

    Adding new projects

---
+++ Basic_C#_Programs/CarInsuranceApprovalProgram/CarInsuranceApprovalProgram/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceApprovalProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Asks if the user is older than 15 and evalutes whether that is true or false.
+            Console.WriteLine("What is your age?");
+            string sAge = Console.ReadLine();
+            int age = Convert.ToInt32(sAge);
+            bool oldEnough = age > 15;
+            //Console.WriteLine(oldEnough);
+
+            // Asks the user if they have a DUI and evaluate whether that is true of false.
+            Console.WriteLine("Have your ever had a DUI?");
+            string sDUI = Console.ReadLine();
+            bool DUI = Convert.ToBoolean(sDUI);
+            bool hasDUI = (DUI == false);
+            //Console.WriteLine(hasDUI);
+
+            // Asks the user if they have more than 3 tickets and evaluates whether that is true of false.
+            Console.WriteLine("How many speeding tickets do you have?");
+            string sTicket = Console.ReadLine();
+            int Ticket = Convert.ToInt32(sTicket);
+            bool Tickets = Ticket <= 3;
+            //Console.WriteLine(Tickets);
+
+            // If all previous conditions are met then the user is qualified.
+            Console.WriteLine("Qualified?");
+            bool qualified = (oldEnough && hasDUI && Tickets);
+            Console.WriteLine(qualified);
+
+            Console.Read();
+        }
+    }
+}
