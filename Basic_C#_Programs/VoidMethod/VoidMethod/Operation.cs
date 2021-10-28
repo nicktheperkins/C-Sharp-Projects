e29a66b3a2e296c13fe5018d9e152422103ea76f
nicktheperkins <nicktheperkins@gmail.com>
2021-10-28 14:35:19 -0600

    Adding a project

---
+++ Basic_C#_Programs/VoidMethod/VoidMethod/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Operation
+    {
+        // This method takes two parameters.
+        // On the first param it performs a math operation and then displays it on the console.
+        // On the second param it just display it to the console.
+        public void Add(int intOne, int intTwo)
+        {
+            Console.WriteLine(intOne + 2);
+            Console.WriteLine(intTwo);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Basic_C#_Programs/VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates my opertion class
+            Operation operation = new Operation();
+
+            // Calls the method and passes in two parameters.
+            operation.Add(1, 2);
+
+            // Calls the method and passes in two parameters by name.
+            operation.Add(intOne: 2, intTwo: 1);
+        }
+    }
+}
