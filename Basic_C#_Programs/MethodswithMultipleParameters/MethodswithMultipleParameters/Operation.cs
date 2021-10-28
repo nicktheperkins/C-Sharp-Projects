be44a8ef2f160fc26ca844f35f8ff290526a475a
nicktheperkins <nicktheperkins@gmail.com>
2021-10-28 14:07:55 -0600

    Adding a project

---
+++ Basic_C#_Programs/MethodswithMultipleParameters/MethodswithMultipleParameters/Operation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MethodswithMultipleParameters
+{
+    class Operation
+    {
+        // This is a method that runs a math operation on two parameters, but the second parameter is optional.
+        public int Add(int numOne, int numTwo = 1)
+        {
+            return (numOne + 2) * numTwo;
+        }
+    }
+}
---
+++ Basic_C#_Programs/MethodswithMultipleParameters/MethodswithMultipleParameters/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MethodswithMultipleParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiates my operations class.
+            Operation operation = new Operation();
+
+            // Asks the user to input a number and converts that number to an integer.
+            Console.WriteLine("Enter a number to add 2 to that number.");
+            int numOne = Convert.ToInt32(Console.ReadLine());
+
+            // Asks the user to input another number if they want.
+            Console.WriteLine("Enter a number to multiply the total from the last entry by. (This is optional, you can leave this blank.)");
+            string numTwo = Console.ReadLine();
+
+            // If the user provides a second number then I call my method with both parameters. If not I call the method with only one of the parameters.
+            if (string.IsNullOrWhiteSpace(numTwo))
+            {
+                int method = operation.Add(numOne);
+                Console.WriteLine("{0} plus 2 equals {1}", numOne, method);
+                Console.ReadLine();
+            }
+            else
+            {
+                int stringTwoNum = Convert.ToInt32(numTwo);
+                int method = operation.Add(numOne, stringTwoNum);
+                Console.WriteLine("{0} plus 2 times {1} equals {2}", numOne, stringTwoNum, method);
+                Console.ReadLine();
+            }
+        }
+    }
+}
