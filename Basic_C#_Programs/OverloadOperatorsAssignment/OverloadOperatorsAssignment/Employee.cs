905aca4d275e6bb89ec56043b3fbd7daa51b8789
nicktheperkins <nicktheperkins@gmail.com>
2021-11-01 11:31:37 -0600

    Adding a project

---
+++ Basic_C#_Programs/OverloadOperatorsAssignment/OverloadOperatorsAssignment/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OverloadOperatorsAssignment
+{
+    public class Employee
+    {   // ID property for my Employee class.
+        public int ID { get; set; }
+
+        // This is an overloaded equality operator. It compares the IDs of the two employees passed into it.
+        public static bool operator ==(Employee e1, Employee e2)
+        {
+            return e1.ID == e2.ID;
+        }
+
+        // This is an overloaded inequality operator.
+        // For the compiler to run the overloaded equality operator without errors the overloaded inequality operator needa to be defined.
+        public static bool operator !=(Employee e1, Employee e2)
+        {
+            return e1.ID == e2.ID;
+        }
+    }
+}
---
+++ Basic_C#_Programs/OverloadOperatorsAssignment/OverloadOperatorsAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OverloadOperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This instantiates and sets the ID property for the two employees I am going to compare.
+            Employee e1 = new Employee { ID = 112233 };
+            Employee e2 = new Employee { ID = 445566 };
+
+            // This uses my overloaded equality operator to compare the IDs of the two employees.
+            bool equality = e1 == e2;
+
+            // This writes and reads my comparison to the console.
+            Console.WriteLine(equality);
+            Console.ReadLine();
+        }
+    }
+}
